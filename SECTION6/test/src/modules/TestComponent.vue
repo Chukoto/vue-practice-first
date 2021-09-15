e63819f68771a5e4d28954fffd8b4f64c13ff981
Haruki Hirai <iamprivateengineer@gmail.com>
2021-09-15 18:01:51 +0900

    [add] wrote local and global scss

--- SECTION6/test/src/modules/TestComponent.vue
+++ SECTION6/test/src/modules/TestComponent.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
+  <div class="red-b">
+    <div class="border__blue">
+      SCSSのテストです
+    </div>
+    <div class="background__primary">
+      SCSSグローバルのテストです。
+    </div>
     テストです
     {{ testData }}
   </div>
@@ -16,4 +22,20 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped lang="scss">
+/* scopedをつけることでコンポーネント内だけで有効になる */
+
+.red-b {
+  border: 1px solid red;
+}
+
+.border {
+  &__blue {
+    border: 1px solid blue;
+  }
+}
+
+.background__primary {
+  background-color: $color-primary;
+}
+</style>
