281331458a9101ec62101e424e35f8b44069bb07
Haruki Hirai <iamprivateengineer@gmail.com>
2021-09-16 18:36:16 +0900

    [mod] cut a part of App.vue, pasted Header.vue

--- SECTION8/bookapp/src/global/Header.vue
+++ SECTION8/bookapp/src/global/Header.vue
@@ -1,13 +1,15 @@
 <template>
-  <div>ヘッダー</div>
+  <div>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center"></div>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
