d99f22ed2bba6fd7274c568400eab34fc112002b
Haruki Hirai <iamprivateengineer@gmail.com>
2021-09-16 18:51:57 +0900

    [add] imported components Header and Footer from global directory in App.vue

--- SECTION8/bookapp/src/App.vue
+++ SECTION8/bookapp/src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <v-app>
-
+    <Header />
 
     <v-main>
       <router-view />
     </v-main>
+    <Footer />
   </v-app>
 </template>
 
 <script>
+import Header from '@/global/Header';
+import Footer from '@/global/Footer';
 export default {
   name: 'App',
+
+  components: {
+    Header,
+    Footer,
+  },
 
   data: () => ({
     //
